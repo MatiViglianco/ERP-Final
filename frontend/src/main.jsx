95d4e29a3e2a00824398d11ba451ae527b608b29
mativiglianco <matiasviglisnco@gmail.com>
2025-11-23 23:56:48 -0300

    fix: set router basename from BASE_URL for gh-pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
+const basename = import.meta.env.BASE_URL || '/'
+
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+    <BrowserRouter basename={basename} future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
